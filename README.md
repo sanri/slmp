3d01172034e2033905ba16df31f99851b5f1d709
sanri <engineer_tang@qq.com>
2019-12-25 17:57:37 +0800

    20191225

---
+++ README.md
@@ -0,0 +1,2 @@
+# SLMP
+三菱SLMP通信协议的Rust实现
--- src/slmp_core.rs
+++ src/slmp_core.rs
@@ -1,12 +1,20 @@
 use std::net::{IpAddr,Ipv4Addr};
 
+struct Destination{
+  network:u8,           //网络编号
+  station:u8,           //站号
+  module:u16,           //模块IO编号
+  multidrop_station:u8, //多点站号
+}
+
 struct Request{
-  network:u8,
-  station:u8,
-  module:u16,
-  multidrop_station:u8,
+  des:Destination,
 }
 
 struct Response{
-
+  des:Destination,
 }
+
+
+
+
