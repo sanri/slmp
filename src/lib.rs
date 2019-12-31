553b08b60083e4bcb5de7b9379d5a3759715aa7e
sanri <engineer_tang@qq.com>
2019-12-31 17:54:50 +0800

    20191231

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,3 @@
 mod slmp_core;
 
 
-
-
-
-#[cfg(test)]
-mod tests {
-  #[test]
-  fn it_works() {
-      assert_eq!(2 + 2, 4);
-  }
-}
-
-
-
--- src/slmp_core.rs
+++ src/slmp_core.rs
@@ -3,25 +3,26 @@ use std::vec;
 use std::path::Prefix::DeviceNS;
 
 //字软元件
-enum DeviceWord {
+pub(crate) enum DeviceWord {
   D, //数据寄存器 D
 }
 
 //位软元件
-enum DeviceBit{
+pub(crate) enum DeviceBit{
   X, //输入继电器 X
   Y, //输出继电器 Y
   M, //内部继电器 M
 }
 
 
-trait Request{
+pub(crate) trait Request{
   //序列化
   fn serialize(&self)->Vec<u8>;
 }
 
-trait Response{
-//反序列化 Deserialization
+pub(crate) trait Response {
+  //反序列化 Deserialization
+  fn deserialization(data: &[u8]) -> Option<Self>;
 }
 
 //批量读 字
@@ -34,8 +35,8 @@ fn read_bits(device:DeviceBit,head_number:u32,number:u16){
 
 }
 
-const requset:[u8;2]=[0x50,0x00];
-const response:[u8;2]=[0xD0,0x00];
+const REQUSET:[u8;2]=[0x50,0x00];
+const RESPONSE:[u8;2]=[0xD0,0x00];
 
 //软元件代码
 enum DeviceCode{
@@ -45,7 +46,7 @@ enum DeviceCode{
   D = 0xA8, //数据寄存器 D
 }
 
-struct Destination{
+pub(crate) struct Destination{
   network:u8,           //网络编号
   station:u8,           //站号
   module:u16,           //模块IO编号
@@ -53,7 +54,7 @@ struct Destination{
 }
 
 impl Destination {
-  fn new()->Destination {
+  pub(crate) fn new() -> Destination {
     Destination {
       network: 0x00,
       station: 0xff,
@@ -73,10 +74,23 @@ impl Destination {
     out[4] = self.multidrop_station;
     return out;
   }
+
+  fn deserialization(data: &[u8]) -> Option<Self> {
+    //网络编号(1) + 站号(1) + 模块编号(2) + 多点站号(1) = 5 字节
+    if data.len() < 5 {
+      return Option::None;
+    }
+    let mut out: Destination = Destination { network: 0, station: 0, module: 0, multidrop_station: 0 };
+    out.network = data[0];
+    out.station = data[1];
+    out.module = d2.from_le_bytes([data[2], data[3]]);
+    out.multidrop_station = data[4];
+    return Option::Some(out);
+  }
 }
 
 //批量读请求(字软元件)
-struct ReqReadWords{
+pub(crate) struct ReqReadWords{
   des:Destination,
   device:DeviceWord,
   head_number:u32,
@@ -86,14 +100,20 @@ struct ReqReadWords{
 impl Request for ReqReadWords {
   fn serialize(&self) -> Vec<u8> {
     let mut out: Vec<u8> = Vec::with_capacity(30);
-    for &i in &requset{
+    //副帧头
+    for &i in &REQUSET {
       out.push(i);
     }
-    let d = Destination::new().serialize();
-    for &i in &d{
-      out.push(d[i]);
+    //目标地址
+    let d = self.des.serialize();
+    for &i in &d {
+      out.push(i);
     }
-    for i in 0..4{
+    //请求数据长
+    out.push(0x0c);
+    out.push(0x0);
+    //保留
+    for i in 0..2 {
       out.push(0);
     }
     //指令
@@ -104,22 +124,41 @@ impl Request for ReqReadWords {
     out.push(0x00);
     //起始软元件编号
     let h = self.head_number.to_le_bytes();
-    for i in 0..3{
+    for i in 0..3 {
       out.push(h[i]);
     }
     //软元件代码
     out.push(DeviceCode::D as u8);
-
+    //软元件点数
+    let n = self.number.to_le_bytes();
+    for i in 0..2 {
+      out.push(n[i]);
+    }
 
     out
   }
 }
 
+#[test]
+fn test_req_read_words_serialize() {
+  let mut d: ReqReadWords = ReqReadWords {
+    des: Destination::new(),
+    device: DeviceWord::D,
+    head_number: 1,
+    number: 1,
+  };
+
+  print!("ReqReadWords:");
+  for i in d.serialize() {
+    print!(" {:#04X}", i);
+  }
+}
 
 
-//批量读响应
-struct ResReadWords{
-  des:Destination,
+//批量读响应(字软元件)
+struct ResReadWords {
+  des: Destination,
+  data: std::result::Result<Vec<u16>, u16>,
 }
 
 
